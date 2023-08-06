a3f695ea0ada04e56c9267794c200bc481bcd233
Alan <a4521789@gmail.com>
2023-08-07 00:44:02 +0800

    Project - update hibernate.cfg.xml, context, maven, core

---
+++ src/main/java/core/dao/CoreDao.java
@@ -0,0 +1,24 @@
+package core.dao;
+
+import java.util.List;
+
+import org.hibernate.Session;
+
+import static core.util.HibernateUtil.getSessionFactory;
+
+public interface CoreDao<P, I> {
+	int insert(P pojo);
+
+	int deleteById(I id);
+
+	int update(P pojo);
+
+	P selectById(I id);
+
+	List<P> selectAll();
+
+	default Session getSession() {
+		return getSessionFactory().getCurrentSession();
+//		return getSessionFactory().openSession();
+	}
+}
