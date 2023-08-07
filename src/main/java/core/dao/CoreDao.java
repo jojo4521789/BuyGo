1bae1b4f92ea759aa5665fc3cdcdaca9551cccc0
Peggy <hahaaa851224@gmail.com>
2023-08-07 15:29:10 +0800

    core - modify "P pojo" to "E Entity"

--- src/main/java/core/dao/CoreDao.java
+++ src/main/java/core/dao/CoreDao.java
@@ -6,16 +6,16 @@ import org.hibernate.Session;
 
 import static core.util.HibernateUtil.getSessionFactory;
 
-public interface CoreDao<P, I> {
-	int insert(P pojo);
+public interface CoreDao<E, I> {
+	int insert(E entity);
 
 	int deleteById(I id);
 
-	int update(P pojo);
+	int update(E entity);
 
-	P selectById(I id);
+	E selectById(I id);
 
-	List<P> selectAll();
+	List<E> selectAll();
 
 	default Session getSession() {
 		return getSessionFactory().getCurrentSession();
