5d1546631ee01c8c6f6f3b377474933c484e2bcd
Peggy <hahaaa851224@gmail.com>
2023-08-07 21:21:10 +0800

    opa coupon - add service & controller

--- src/main/java/web/back_end/opa/coupon/dao/impl/CouponDaoImpl.java
+++ src/main/java/web/back_end/opa/coupon/dao/impl/CouponDaoImpl.java
@@ -1,6 +1,10 @@
 package web.back_end.opa.coupon.dao.impl;
 
+import java.sql.Timestamp;
 import java.util.List;
+
+import org.hibernate.Session;
+import org.hibernate.query.Query;
 
 import web.back_end.opa.coupon.dao.CouponDao;
 import web.back_end.opa.coupon.entity.Coupon;
@@ -14,33 +18,51 @@ public class CouponDaoImpl implements CouponDao{
 	}
 
 	@Override
-	public int deleteById(Integer id) {
-		// TODO Auto-generated method stub
-		return 0;
+	public int deleteById(Integer opaCouponNo) {
+		Session session = getSession();
+		Coupon coupon = session.get(Coupon.class, opaCouponNo);
+		session.remove(coupon);
+		return 1;
 	}
 
 	@Override
 	public int update(Coupon coupon) {
-		// TODO Auto-generated method stub
-		return 0;
+		Session session = getSession();
+		Coupon oldCoupon = session.get(Coupon.class, coupon.getOpaCouponNo());
+		final String opaCouponName = coupon.getOpaCouponName();
+		if(opaCouponName != null) {
+			oldCoupon.setOpaCouponName(opaCouponName);
+		}
+		final Integer opaDiscountAmo = coupon.getOpaDiscountAmo();
+		if(opaDiscountAmo != null) {
+			oldCoupon.setOpaDiscountAmo(opaDiscountAmo);
+		}
+		final Integer opaMinAmount = coupon.getOpaMinAmount();
+		if(opaMinAmount != null) {
+			oldCoupon.setOpaMinAmount(opaMinAmount);
+		}
+		final Timestamp opaExpDate = coupon.getOpaExpDate();
+		if(opaExpDate != null) {
+			oldCoupon.setOpaExpDate(opaExpDate);
+		}
+		return 1;
 	}
 
 	@Override
 	public Coupon selectById(Integer id) {
-		// TODO Auto-generated method stub
-		return null;
+		return getSession().get(Coupon.class, id);
 	}
 
 	@Override
 	public List<Coupon> selectAll() {
-		// TODO Auto-generated method stub
-		return null;
+		final String hql = "FROM Coupon ORDER BY opaCouponNo";
+		return getSession().createQuery(hql, Coupon.class).getResultList();
 	}
 
 	@Override
 	public Coupon selectByOpaCouponName(String opaCouponName) {
-		// TODO Auto-generated method stub
-		return null;
+		Query<Coupon> query = getSession().createQuery("FROM Coupon WHERE opaCouponName=" + opaCouponName, Coupon.class);
+		return query.getSingleResult();
 	}
 
 }
