43bf6fcbd06c8a198451d2c63f7cb2c784237025
Peggy <hahaaa851224@gmail.com>
2023-09-06 15:53:31 +0800

    add ECPay SDK

---
+++ src/main/java/web/front_end/member/opa/checkout/util/ECPayCheckoutConstants.java
@@ -0,0 +1,8 @@
+package web.front_end.member.opa.checkout.util;
+
+import web.front_end.member.opa.checkout.service.ECPayCheckoutService;
+import web.front_end.member.opa.checkout.service.impl.ECPayCheckoutServiceImpl;
+
+public class ECPayCheckoutConstants {
+	public static final ECPayCheckoutService SERVICE = new ECPayCheckoutServiceImpl();
+}
