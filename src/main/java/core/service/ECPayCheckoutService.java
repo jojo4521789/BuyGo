fea2cf93ed4b357488e9f01e26fef8c563c5f112
Peggy <hahaaa851224@gmail.com>
2023-09-09 01:08:16 +0800

    ecpay - modify controller & service

--- src/main/java/core/service/ECPayCheckoutService.java
+++ src/main/java/core/service/ECPayCheckoutService.java
@@ -1,30 +1,27 @@
-package web.front_end.member.opa.checkout.service.impl;
+package core.service;
 
 import java.text.SimpleDateFormat;
 import java.util.UUID;
 
 import ecpay.payment.integration.AllInOne;
 import ecpay.payment.integration.domain.AioCheckOutALL;
-import web.front_end.member.opa.checkout.service.ECPayCheckoutService;
 
-public class ECPayCheckoutServiceImpl implements ECPayCheckoutService {
-
-	@Override
-	public String getECpayForm(AioCheckOutALL aioCheckOutALL) {
+public class ECPayCheckoutService {
+	public String getECpayForm(AioCheckOutALL aioCheckOutALL, String returnURL) {
 		// java.util.Date → String
 		java.util.Date tradeDate = new java.util.Date();
 		SimpleDateFormat sdf = new SimpleDateFormat("yyyy/MM/dd HH:mm:ss");
 		String tradeDateStr = sdf.format(tradeDate);
-
+		
+		//使用 UUID 隨機產生 20 碼的亂數
 		String uuId = UUID.randomUUID().toString().replaceAll("-", "").substring(0, 20);
 		AllInOne all = new AllInOne("");
 
 		aioCheckOutALL.setMerchantTradeNo(uuId);
 		aioCheckOutALL.setMerchantTradeDate(tradeDateStr);
-		aioCheckOutALL.setReturnURL("http://211.23.128.214:5000");
+		aioCheckOutALL.setReturnURL(returnURL);
 		aioCheckOutALL.setNeedExtraPaidInfo("N");
 		String form = all.aioCheckOut(aioCheckOutALL, null);
 		return form;
 	}
-
 }
--- src/main/java/web/front_end/member/opa/checkout/util/ECPayCheckoutConstants.java
+++
@@ -1,8 +0,0 @@
-package web.front_end.member.opa.checkout.util;
-
-import web.front_end.member.opa.checkout.service.ECPayCheckoutService;
-import web.front_end.member.opa.checkout.service.impl.ECPayCheckoutServiceImpl;
-
-public class ECPayCheckoutConstants {
-	public static final ECPayCheckoutService SERVICE = new ECPayCheckoutServiceImpl();
-}
