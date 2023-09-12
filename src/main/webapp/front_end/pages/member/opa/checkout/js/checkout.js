5d399b5534951a0682d57fa1f293db0d6abc9a12
Peggy <hahaaa851224@gmail.com>
2023-09-12 21:28:23 +0800

    opa checkout

--- src/main/webapp/front_end/pages/member/opa/checkout/js/checkout.js
+++ src/main/webapp/front_end/pages/member/opa/checkout/js/checkout.js
@@ -7,6 +7,7 @@ $("#discount").text(cartData.opaDiscountAmo);
 
 let itemName = "";
 for (let prod of cartData.prods) {
+    //載入結帳商品清單
     fetch("/BuyGo/api/opa/prod/selectById",
         {
             method: "POST",
@@ -17,7 +18,7 @@ for (let prod of cartData.prods) {
         })
         .then(resp => resp.json())
         .then(data => {
-            itemName = itemName + data.opaProdName + " ";
+            itemName = itemName + data.opaProdName + ", ";
             let img_url = "../../../../img/common/Image_not_available.png";
             if (data.prpicsList.length !== 0) {
                 img_url = data.prpicsList[0].opaProdPicture;
@@ -48,7 +49,9 @@ for (let prod of cartData.prods) {
 
 $("#confirmCheckoutBtn").on("click", function () {
 
-    fetch("/BuyGo/opa/checkout/ecpay",
+    //結帳，但應該要變成立訂單比較合理
+    //CustomField1=給後端判斷用的action，CustomField2=專案的orderId
+    fetch("/BuyGo/ecpay/checkout",
         {
             method: "POST",
             headers: { "Content-Type": "application/json; charset=utf-8", },
@@ -56,10 +59,36 @@ $("#confirmCheckoutBtn").on("click", function () {
                 TotalAmount: cartData.cartGrandTTL,
                 TradeDesc: "BuyGo官方代購",
                 ItemName: itemName,
+                CustomField1: "opaOrderFirstCheckout",
+                CustomField2: "orderId"
             })
         }).then(resp => resp.json())
         .then(data => {
             console.log(data);
             window.open(data);
         });;
+
+    //刪除會員購物車資料
+    for (let prod of cartData.prods){
+        memberCartProdRemove(prod.opaProdNo);
+    }
 });
+
+//刪除會員購物車資料
+function memberCartProdRemove(opaProdNo) {
+    let opaCartId = {
+        memberNo: null,
+        opaProdNo: opaProdNo
+    };
+    fetch("/BuyGo/needLoginApi/opa/cart/remove", {
+        method: "POST",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify({ opaCartId })
+    })
+        .then(resp => resp.json())
+        .then(body => {
+            if (!body.successful) {
+                console.log("移除購物車明細失敗");
+            }
+        });
+}
