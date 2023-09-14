b71f984c964d9e4f36bfc13b641805298fef8527
suwinin <suwinin49@gmail.com>
2023-09-14 23:38:22 +0800

    commit fixed order and request notification

--- src/main/java/web/back_end/opa/order/controller/AdminOrderNotificationServlet.java
+++ src/main/java/web/back_end/opa/order/controller/AdminOrderNotificationServlet.java
@@ -29,65 +29,64 @@ import javax.mail.internet.MimeMessage;
 
 @WebServlet("/needLoginApi/admin/opa/order/notify")
 public class AdminOrderNotificationServlet extends HttpServlet {
-	private static final long serialVersionUID = 1L;
-
-	@Override
-	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws IOException {
-		final Core core = new Core();
-		response.setCharacterEncoding("UTF-8");
-		if (request.getParameter("id") == null) {
-			core.setSuccessful(false);
-			core.setMessage("invalid parameter");
-			writePojo2Json(response, core);
-			return;
-		}
-		int id = Integer.parseInt(request.getParameter("id"));
-		try {
-			Notification notification = web.front_end.member.opa.order.util.OpaOrderConstants.SERVICE.getCancelNotification(id);
-			Integer inserted_id = web.front_end.member.notification.util.NotificationConstants.SERVICE.insert(notification);
-			String error = sendEmailNotification(
-				notification.getNotifiTitle(),
-				notification.getNotifiContent(),
-				notification.getMemberNo(),
-				core
-			);
-			core.setSuccessful(error == null);
-		} catch (Exception e) {
-			core.setSuccessful(false);
+ private static final long serialVersionUID = 1L;
+@Override
+ protected void doPost(HttpServletRequest request, HttpServletResponse response) throws IOException {
+  final Core core = new Core();
+  response.setCharacterEncoding("UTF-8");
+  if (request.getParameter("id") == null) {
+   core.setSuccessful(false);
+   core.setMessage("invalid parameter");
+   writePojo2Json(response, core);
+   return;
+  }
+  int id = Integer.parseInt(request.getParameter("id"));
+  try {
+   Notification notification = web.front_end.member.opa.order.util.OpaOrderConstants.SERVICE.getCancelNotification(id);
+   Integer inserted_id = web.front_end.member.notification.util.NotificationConstants.SERVICE.insert(notification);
+   String error = sendEmailNotification(
+    notification.getNotifiTitle(),
+    notification.getNotifiContent(),
+    notification.getMemberNo(),
+    core
+   );
+   core.setSuccessful(error == null);
+core.setMessage(error);
+  } catch (Exception e) {
+   core.setSuccessful(false);
             core.setMessage(e.getMessage());
         }
-		writePojo2Json(response, core);
-	}
-	
-	private String sendEmailNotification(String subject, String messageText, int memberNo, Core core) {
-		try {
-			Properties props = new Properties();
+  writePojo2Json(response, core);
+ }
+ 
+ private String sendEmailNotification(String subject, String messageText, int memberNo, Core core) {
+  try {
+   Properties props = new Properties();
+   props.put("mail.smtp.host", "smtp.gmail.com");
+   props.put("mail.smtp.socketFactory.port", "465");
+   props.put("mail.smtp.socketFactory.class", "javax.net.ssl.SSLSocketFactory");
+   props.put("mail.smtp.auth", "true");
+   props.put("mail.smtp.port", "465");
 
-			props.put("mail.smtp.host", "smtp.gmail.com");
-			props.put("mail.smtp.starttls.enable","true");
-			props.put("mail.smtp.auth", "true");
-			props.put("mail.smtp.ssl.protocols", "TLSv1.2");
+   final String myGmail = "tibamecha102g6@gmail.com";
+   final String myGmail_password = "vwnzqhlqdujkuhwz";
+Session session = Session.getInstance(props, new Authenticator() {
+    protected PasswordAuthentication getPasswordAuthentication() {
+     return new PasswordAuthentication(myGmail, myGmail_password);
+    }
+   });  
+   String email = web.front_end.member.acc.util.MemberConstants.SERVICE.selectById(memberNo).getMemberEmail();
+   Message message = new MimeMessage(session);
+   message.setFrom(new InternetAddress(myGmail));
+   message.setRecipients(Message.RecipientType.TO, InternetAddress.parse(email));
 
-			final String myGmail = "tibamecha102g6@gmail.com";
-			final String myGmail_password = "vwnzqhlqdujkuhwz";
-
-			Session session = Session.getInstance(props, new Authenticator() {
-				protected PasswordAuthentication getPasswordAuthentication() {
-					return new PasswordAuthentication(myGmail, myGmail_password);
-				}
-			});		
-			String email = web.front_end.member.acc.util.MemberConstants.SERVICE.selectById(memberNo).getMemberEmail();
-			Message message = new MimeMessage(session);
-			message.setFrom(new InternetAddress(myGmail));
-			message.setRecipients(Message.RecipientType.TO, InternetAddress.parse(email));
-
-			message.setSubject(subject);
-			message.setText(messageText);
-			Transport.send(message);
-			core.setMessage("已通知 " + email);
-			return null;
-		} catch (MessagingException e) {
-			return e.getMessage();
-		}
-	}
+   message.setSubject(subject);
+   message.setText(messageText);
+   Transport.send(message);
+   core.setMessage("已通知 " + email);
+   return null;
+  } catch (MessagingException e) {
+   return e.getMessage();
+  }
+ }
 }
